258c1b80cc95dcd299f9215bf07c29a28934749b
Maycon Oliveira <mayconbrito1998@hotmail.com>
2023-10-16 16:19:23 -0300

    Adicionado styled-component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import './globals.css';
+import StyledJsxRegistry from '@/lib/registry';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
@@ -12,7 +12,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <StyledJsxRegistry>{children}</StyledJsxRegistry>
+      </body>
     </html>
   );
 }
--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -5,7 +5,7 @@ describe('<Main />', () => {
   it('should render the heading', () => {
     const { container } = render(<Main />);
 
-    expect(screen.getByRole('heading', { name: /Main/i })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: /React Avançado/i })).toBeInTheDocument();
 
     expect(container.firstChild).toMatchSnapshot();
   });
